4aa39d3be0b9166cf0235cb00eb3b5cb377e90e0
Alexander Nwala <anwala@cs.odu.edu>
2016-08-12 16:37:19 -0400

    nesting reduction, error checking

--- LocalMemoryChromeExn/scripts/getGoogleLinksDict.js
+++ LocalMemoryChromeExn/scripts/getGoogleLinksDict.js
@@ -1,3 +1,5 @@
+
+
 googleRetrieveLinksFromPage(document);
 
 //scraper
@@ -19,41 +21,43 @@ function googleRetrieveLinksFromPage(document)
 		for(var j=0; j<liOrDivs.length; j++)
 		{
 			var liOrDiv = liOrDivs[j];
-			if( liOrDiv.tagName === 'DIV' || liOrDiv.tagName === 'LI' )
-			{	
-				var snippet = liOrDiv.getElementsByClassName('st');
-				var titleLink = liOrDiv.getElementsByTagName('h3');
-				
-				if( snippet.length != 0 )
+			if( liOrDiv.tagName !== 'DIV' && liOrDiv.tagName !== 'LI' )
+			{
+				continue;
+			}
+
+			var snippet = liOrDiv.getElementsByClassName('st');
+			var titleLink = liOrDiv.getElementsByTagName('h3');
+			
+			if( snippet.length != 0 )
+			{
+				if( snippet[0].tagName === 'SPAN' )
 				{
-					if( snippet[0].tagName === 'SPAN' )
+					snippet = snippet[0].innerHTML;
+					snippetText = snippet;
+
+					var indexOfEndSpan = snippet.indexOf('</span>');
+					if( indexOfEndSpan != -1 )
 					{
-						snippet = snippet[0].innerHTML;
+						//</span> is 7
+						crawlDateTime = snippet.substr(0, indexOfEndSpan+7);
+						
+						//snippet contains crawlDateTime
+						snippet = snippet.replace(crawlDateTime, '');
 						snippetText = snippet;
 
-						var indexOfEndSpan = snippet.indexOf('</span>');
-						if( indexOfEndSpan != -1 )
-						{
-							//</span> is 7
-							crawlDateTime = snippet.substr(0, indexOfEndSpan+7);
-							
-							//snippet contains crawlDateTime
-							snippet = snippet.replace(crawlDateTime, '');
-							snippetText = snippet;
-
-							//remove tags from crawlDateTime
-							crawlDateTime = crawlDateTime.replace('<span class="f">', '');
-							crawlDateTime = crawlDateTime.replace(' - </span>', '');
-						}
+						//remove tags from crawlDateTime
+						crawlDateTime = crawlDateTime.replace('<span class="f">', '');
+						crawlDateTime = crawlDateTime.replace(' - </span>', '');
 					}
 				}
+			}
 
-				if( titleLink.length != 0 )
-				{
-					titleLink = titleLink[0].getElementsByTagName('a');
-					title = titleLink[0].innerHTML;
-					link = titleLink[0].href;
-				}
+			if( titleLink.length != 0 )
+			{
+				titleLink = titleLink[0].getElementsByTagName('a');
+				title = titleLink[0].innerHTML;
+				link = titleLink[0].href;
 
 				payload[link] = {title: title, crawlDatetime: crawlDateTime, snippet: snippetText};
 			}
